5ba12293cd46501e39de7e78b5c2a1f2297f296c
AleksandraRezetka <olarezetka@gmail.com>
2020-04-25 01:13:05 +0200

    Added TCP Server Class

---
+++ src/main/java/TCP/TCPServer.java
@@ -0,0 +1,45 @@
+package TCP;
+import workers.CreateWorker;
+import workers.Worker;
+import workers.WorkersList;
+import java.io.*;
+import java.net.*;
+import java.util.List;
+
+
+public class TCPServer {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private ObjectInputStream in;
+    
+    public void start(int port) {
+        try{
+            serverSocket = new ServerSocket(port);
+            clientSocket = serverSocket.accept();
+            try {
+                in = new ObjectInputStream(clientSocket.getInputStream());
+                WorkersList workers = (WorkersList) in.readObject();
+                for(Worker w : workers.getWorkers()){
+                    System.out.print(w.toString()+"/n");
+                }
+                in.close();
+            }catch (IOException | ClassNotFoundException f) {
+                System.out.print("Sth is wrong with deserializaton");
+            }
+            clientSocket.close();
+            serverSocket.close();
+        }catch (IOException e){
+            System.out.println("Unable to create socket");
+        }
+    }
+    public static void main(String[] args){
+        CreateWorker cr = new CreateWorker();
+        String fileName = "Data\\FT1.json";
+        List<Worker> workers = cr.createFullTimeWorker(fileName);
+        WorkersList w = new WorkersList();
+        w.setWorkers(workers);
+        TCPServer server = new TCPServer();
+        server.start(6666);
+        System.out.print("Server was created");
+    }
+}
